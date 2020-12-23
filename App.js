719966817a954167766ce953e51964f80cc5d440
aadityarachmat <adityarachmat766@gmail.com>
2020-12-23 16:40:59 +0800

    fixed the date

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import * as firebase from "firebase";
+
+import MainNavigator from "./src/screens/MainNavigator";
+import LoginScreen from "./src/screens/LoginScreen";
+import SignupScreen from "./src/screens/SignupScreen";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDQp7-Jl-NDbtDszNGWUjmknxoII2a19NU",
+  authDomain: "tfl-mvp2.firebaseapp.com",
+  databaseURL: "https://tfl-mvp2.firebaseio.com",
+  projectId: "tfl-mvp2",
+  storageBucket: "tfl-mvp2.appspot.com",
+  messagingSenderId: "400517861923",
+  appId: "1:400517861923:web:5b37471c11a51b6c9e9796",
+  measurementId: "G-QQW6WBFX80",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const Stack = createStackNavigator();
+
+class App extends React.Component {
+  state = {
+    loggedIn: false,
+  };
+
+  componentDidMount() {
+    this.checkUserListener();
+  }
+
+  checkUserListener = () => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  };
+
+  render() {
+    const { loggedIn } = this.state;
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Login"
+          screenOptions={{ headerShown: false }}
+        >
+          {loggedIn && <Stack.Screen name="Main" component={MainNavigator} />}
+          {!loggedIn && (
+            <>
+              <Stack.Screen name="Login" component={LoginScreen} />
+              <Stack.Screen name="Signup" component={SignupScreen} />
+            </>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
